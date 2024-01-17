f68647dd5eaf6b97bd7126070a5c9355ed6da355
Sachin Gupta <sgupta15120@gmail.com>
2024-01-18 00:15:05 +0530

    modified

--- the-yummy-club/src/pages/Home.jsx
+++ the-yummy-club/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import CategoryMenu from '../components/CategoryMenu';
+import FoodItems from '../components/FoodItems';
+import Cart from '../components/Cart';
 
 const Home = () => {
     return (
         <div>
-           
             <NavBar />
+            <CategoryMenu />
+            <FoodItems />
+            <Cart/>
         </div>
     );
 }
