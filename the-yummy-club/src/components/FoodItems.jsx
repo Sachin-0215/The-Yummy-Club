2ad515d640f971ef5460cc45b78a80080313ff91
Sachin Gupta <sgupta15120@gmail.com>
2024-01-18 00:13:00 +0530

    food items added

---
+++ the-yummy-club/src/components/FoodItems.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import FoodCard from './FoodCard'
+import FoodData from '../data/FoodData.js'
+
+const FoodItems = () => {
+  return (
+      <div className='flex flex-wrap gap-10 justify-center lg:justify-start mx-6 my-10'>
+          {
+              FoodData.map((food) => {
+                  return (
+                      <FoodCard
+                      key={food.id}
+                      id={food.id}
+                      name={food.name}
+                      price={food.price}
+                      descr={food.desc}
+                      rating={food.rating}
+                      img={food.img}
+                  />
+                  );
+              })}
+        <FoodCard/>
+    </div>
+  )
+}
+
+export default FoodItems
