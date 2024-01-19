1fbc565f28bab0ef5e7b751004fb827176c5c39c
Sachin Gupta <sgupta15120@gmail.com>
2024-01-19 14:32:12 +0530

    food cards created

--- the-yummy-club/src/components/FoodCard.jsx
+++ the-yummy-club/src/components/FoodCard.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { FaStar } from "react-icons/fa";
+import {  useDispatch } from 'react-redux';
+import { addToCart } from '../redux/slices/CartSlice';
 
 const FoodCard = ({ id, img, name, price, descr, rating }) => {
-  
+
+  const dispatch = useDispatch();
+
   return (
     <div className='font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2 '>
       <img className='w-auto h-[130px] hover:scale-110 cursor-grab transition-all duration-500 ease-in-out' src={img}
@@ -16,7 +20,9 @@ const FoodCard = ({ id, img, name, price, descr, rating }) => {
       <p className='text-sm font-normal'>  { descr } </p>
       <div className='flex justify-between'>
           <span className='flex justify-center items-center'><FaStar className='mr-1 text-yellow-400'/> {rating}</span>
-          <button className='p-1 text-white bg-green-400 hover:bg-green-600 rounded-lg text-sm'>Add to Cart</button>
+        <button onClick={() => {
+              dispatch(addToCart({id,name,price,rating,price,img,qty:1}))
+          }} className='p-1 text-white bg-green-400 hover:bg-green-600 rounded-lg text-sm'>Add to Cart</button>
       </div>
     </div>
   )
