b9892442b703ebf995c471550cfedadd5c2e449c
Sachin Gupta <sgupta15120@gmail.com>
2024-02-02 23:18:57 +0530

    Authentication added

--- the-yummy-club/src/main.jsx
+++ the-yummy-club/src/main.jsx
@@ -4,12 +4,20 @@ import App from './App.jsx'
 import './index.css';
 import { Provider } from 'react-redux';
 import Store from './redux/Store.js';
-
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={Store}>
-        <App />
-    </Provider>
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-illd28ipycnh0asn.us.auth0.com"
+    clientId="vuAZaUfYFqxwdgeuWT9yURe9NTqyAsjf"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+      <React.StrictMode>
+        <Provider store={Store}>
+            <App />
+        </Provider>
+      </React.StrictMode>,
+  </Auth0Provider>
 )
